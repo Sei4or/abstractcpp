541d910afe5f413dc47da69e6dbfcd8756dfb0dd
Sei4or <sei4or@gmail.com>
2020-02-07 18:17:15 -0800

    Array primitives and 2D array primitives

--- include/abstractcpp/arrays.hpp
+++ include/abstractcpp/arrays.hpp
@@ -8,6 +8,22 @@ namespace abstractcpp
 	{
 	public:
 		static std::string to_string(const std::vector<int>& arr);
+		static std::string to_string(const std::vector<unsigned int>& arr);
+		static std::string to_string(const std::vector<long>& arr);
+		static std::string to_string(const std::vector<unsigned long>& arr);
+		static std::string to_string(const std::vector<long long>& arr);
+		static std::string to_string(const std::vector<unsigned long long>& arr);
+		static std::string to_string(const std::vector<float>& arr);
+		static std::string to_string(const std::vector<double>& arr);
+		static std::string to_string(const std::vector<long double>& arr);
 		static std::string deep_to_string(const std::vector< std::vector<int> >& arr);
+		static std::string deep_to_string(const std::vector< std::vector<unsigned int> >& arr);
+		static std::string deep_to_string(const std::vector< std::vector<long> >& arr);
+		static std::string deep_to_string(const std::vector< std::vector<unsigned long> >& arr);
+		static std::string deep_to_string(const std::vector< std::vector<long long> >& arr);
+		static std::string deep_to_string(const std::vector< std::vector<unsigned long long> >& arr);
+		static std::string deep_to_string(const std::vector< std::vector<float> >& arr);
+		static std::string deep_to_string(const std::vector< std::vector<double> >& arr);
+		static std::string deep_to_string(const std::vector< std::vector<long double> >& arr);
 	};
 }
